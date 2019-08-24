5b0fbf9984a3a7c1577c693134d5f71cde6e1d38
김은비 <biiya0807@gmail.com>
2019-08-24 14:59:07 +0900

    URLSession 생성

--- JSONParsing20190824/ViewController.swift
+++ JSONParsing20190824/ViewController.swift
@@ -1,19 +1,48 @@
-//
-//  ViewController.swift
-//  JSONParsing20190824
-//
-//  Created by 김은비 on 24/08/2019.
-//  Copyright © 2019 biiyasoft. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // https://sncdust.blob.core.windows.net/sncblob/muinbal.json
+    
+    // 배열생성
+    var jsonArr = [String]() //  json 전체를 담는 배열
+    var jibunArr = [String]() // jibun만 담을 배열
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        getDataFromServer()
+        
     }
+    
+    func getDataFromServer() {
+        // urlsession api
+        let configuration = URLSessionConfiguration.default
+        let session = URLSession(configuration: configuration)
+        let request = NSMutableURLRequest(url: URL(string: "https://sncdust.blob.core.windows.net/sncblob/muinbal.json")!)
+        request.httpMethod = "GET"
+        
+        let task = session.dataTask(with: request as URLRequest) { (data, response, error) in
+                print("data: \(data!)")
+                print("response: \(response!)")
+                print("error: \(error)")
+            if error == nil {
+                // JSON Parsing
+                
+                
+                
+            } else {
+                print("error")
+            }
+            
+        }
+        task.resume()
+        
+    }
+    
 
 
 }
